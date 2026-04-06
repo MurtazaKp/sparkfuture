33b54bef56c9813d5181b8ed3735f8b691ad974c
MurtazaKp <Khopoliwala52@gmail.com>
2026-04-06 19:49:43 +0530

    fix the not found page

--- src/components/pages/error/index.tsx
+++ src/components/pages/error/index.tsx
@@ -1,19 +1,17 @@
-import BreadCrumb from "@/components/common/BreadCrumb"
-import FooterOne from "@/layouts/footers/FooterOne"
-import HeaderOne from "@/layouts/headers/HeaderOne"
-import ErrorArea from "./ErrorArea"
+import BreadCrumb from "@/components/common/BreadCrumb";
+import FooterOne from "@/layouts/footers/FooterOne";
+import HeaderOne from "@/layouts/headers/HeaderOne";
+import ErrorArea from "./ErrorArea";
 
 const NotFound = () => {
-   return (
-      <>
-         <HeaderOne />
-         <main>
-            <BreadCrumb sub_title="Page Not Found" title="404" />
-            <ErrorArea />
-         </main>
-         <FooterOne />
-      </>
-   )
-}
+  return (
+    <>
+      <main>
+        <BreadCrumb sub_title="Page Not Found" title="404" />
+        <ErrorArea />
+      </main>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
